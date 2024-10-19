4ec07bc7d5e362efb2435508ff45cf8794f86f75
cjeanm <jeanmichelcid@gmail.com>
2024-10-19 00:36:48 -0400

    tick stuff
    index
    
    wed
    
    wd

--- src/event-loop/index.ts
+++ src/event-loop/index.ts
@@ -106,6 +106,33 @@ async function ex2(): Promise<void> {
 async function main(): Promise<void> {
     await ex1();
     await ex2();
+    await ex3();
 }
 
 main();
+
+function monitorPerformance<T extends (...args: any[]) => Promise<any>>(fn: T): T {
+    return async function(...args: Parameters<T>): Promise<ReturnType<T>> {
+        await tick();
+        const start = performance.now(); 
+        const result = await fn(...args);
+        const end = performance.now();
+        console.log(`Execution time for ${fn.name}: ${end - start}ms`); // Log execution time
+        return result;
+    } as T;
+}
+
+
+async function fetchDataJson(): Promise<any> {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    return await response.json(); 
+}
+
+async function ex3(): Promise<void> {
+    const monitoredFetchData = monitorPerformance(fetchDataJson);
+    console.log();
+    console.log("ex 3");
+    console.log();
+    console.log("Fetching data...");
+    const data = await monitoredFetchData(); // Call the monitored function
+}
